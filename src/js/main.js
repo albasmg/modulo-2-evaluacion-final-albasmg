f23166451b671eee6a4a1a85eb986f4ed4efc5f5
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-07-10 15:22:38 +0200

    add favorite objects to array

--- src/js/main.js
+++ src/js/main.js
@@ -7,13 +7,50 @@ const removeLastSearch = () => {
   resultsContainer.innerHTML = '';
 };
 
+const addListeners = () => {
+  const series = document.querySelectorAll('.main__seriesContainer');
+  for (const li of series) {
+    li.addEventListener('click', handleAddToFavoritesClick);
+  }
+};
+let favoriteSeries = [];
+
+const handleAddToFavoritesClick = (event) => {
+  event.currentTarget.classList.toggle('selected');
+  const selectedSerieImage = event.currentTarget.querySelector('img').src;
+  const selectedSerieName = event.currentTarget.querySelector(
+    '.main__seriesName'
+  ).innerHTML;
+
+  const favoriteSerie = {
+    name: selectedSerieName,
+    image: selectedSerieImage,
+  };
+
+  favoriteSeries.push(favoriteSerie);
+  console.log(favoriteSeries);
+
+  addSerieToFavorites();
+};
+
+const addSerieToFavorites = () => {
+  // const favoritesContainer = document.querySelector('.js-favoritesContainer');
+  // const favoriteSerieContainer = document.createElement('div');
+  // const favoriteSerieImage = document.createElement('img');
+  // const favoriteSerieName = document.createElement('p');
+  // favoritesContainer.appendChild(favoriteSerieContainer);
+  // favoriteSerieContainer.appendChild(favoriteSerieImage);
+  // favoriteSerieContainer.appendChild(favoriteSerieName);
+  // console.log(favoritesContainer);
+};
+
 const printResults = (results) => {
   removeLastSearch();
   for (const result of results) {
     const serieName = result.show.name;
     const serieImage = result.show.image;
 
-    const seriesContainer = document.createElement('div');
+    const seriesContainer = document.createElement('li');
     const serieImageElement = document.createElement('img');
     const serieNameElement = document.createElement('p');
     const serieNameContent = document.createTextNode(`${serieName}`);
@@ -29,7 +66,11 @@ const printResults = (results) => {
         ? 'https://via.placeholder.com/210x295/ffffff/666666/? text=TV'
         : serieImage.medium
     );
+
+    seriesContainer.classList.add('main__seriesContainer');
+    serieNameElement.classList.add('main__seriesName');
   }
+  addListeners();
 };
 
 const handleSearchButtonClick = () => {
