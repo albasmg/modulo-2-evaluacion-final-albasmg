2f198277395301838320f164a76cc00b19edf7b8
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-07-10 09:31:04 +0200

    add API fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('Hola');
+const searchButton = document.querySelector('.js-searchButton');
+
+const handleSearchButtonClick = () => {
+  const searcherValue = document.querySelector('.js-searchInput').value;
+
+  fetch(`http://api.tvmaze.com/search/shows?q=${searcherValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const results = data;
+
+      for (const result of results) {
+        console.log(result.show.name);
+        console.log(result.show.image.medium);
+      }
+    });
+};
+
+searchButton.addEventListener('click', handleSearchButtonClick);
